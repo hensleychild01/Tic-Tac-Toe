114ce6dc628a52178f5c59f7c36e86992d1fd277
hensleychild01 <hensleychild01@gmail.com>
2024-02-21 20:59:19 -0500

    Connect socket

--- host.py
+++ host.py
@@ -1,9 +1,43 @@
+from socket import AF_INET, SOCK_STREAM, socket
+
+try:
+    HOST: str = input("$ Host IP: ").strip()
+    PORT: int = int(input("$ Server port (5050 if unsure): ").strip())
+except TypeError: 
+    print("$ Invalid type")
+    exit()
+
+try: 
+    server: socket = socket(AF_INET, SOCK_STREAM)
+    server.bind((HOST, PORT))
+    print(f"$ Server hosted at {HOST}:{PORT}")
+except: 
+    print(f"$ Server unable to be hosted at {HOST}:{PORT}")
+
+import pygame
 from board import Board
 from settings import win
 
-from socket import AF_INET, SOCK_STREAM, socket
+def main():    
+    board: Board = Board()
 
-HOST, PORT = input("$ Host IP: ").strip(), int(input("$ Server port (5050 by default): ").strip())
-server = socket(AF_INET, SOCK_STREAM)
-server.bind((HOST, PORT))
+    run = True 
+    while run: 
+        for event in pygame.event.get():
+            match event.type: 
+                case pygame.QUIT:
+                    run = False 
+                    break
+                case pygame.MOUSEBUTTONDOWN:
+                    board.make_move(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 
+        win.fill((0, 0, 0))
+
+        board.draw()
+        board.check_for_winner()
+
+        pygame.display.flip()
+    
+    pygame.quit()
+
+main()
