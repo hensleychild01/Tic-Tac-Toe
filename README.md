1992fbdfd3d37239a423a9ce632beae788edf1fa
hensleychild01 <hensleychild01@gmail.com>
2024-02-28 18:20:40 -0500

    Addded reset button and Draw to offline mode

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Tic Tac Toe
 
 ### Working on networking this thing
-
-## Requirements: 
-- pygame
-- socket (included in python)
--- offline.py
+++ offline.py
@@ -1,6 +1,24 @@
 import pygame
 from settings import win, WIDTH, HEIGHT, FONT_SIZE
 
+big_font = pygame.font.SysFont("arial", FONT_SIZE)
+small_font = pygame.font.SysFont("arial", 50)
+
+class ResetButton: 
+        def __init__(self) -> None:
+            self.res = (205, 60)
+            self.pos = ((WIDTH-self.res[0])/2, (HEIGHT-self.res[1])/2)
+
+        def draw(self): 
+            button = pygame.Rect(self.pos[0], self.pos[1], self.res[0], self.res[1])
+            text = small_font.render("Play again!", False, (0, 0, 0))
+            pygame.draw.rect(win, (200, 200, 200), button)
+            win.blit(text, self.pos)
+        
+        def is_clicked(self) -> bool: 
+            mouse_pos = pygame.mouse.get_pos()
+            return True if ((self.pos[0] <= mouse_pos[0] <= self.pos[0] + self.res[0]) and (self.pos[1] <= mouse_pos[1] <= self.pos[1] + self.res[1])) else False
+
 class Board: 
     def __init__(self) -> None:
         self.board: list = [
@@ -9,7 +27,7 @@ class Board:
             [0, 0, 0]
         ]
 
-        self.font = pygame.font.SysFont("arial", FONT_SIZE)
+        self.reset_button = ResetButton()
 
         self.x_enum: int = 1
         self.o_enum: int = 2
@@ -18,9 +36,22 @@ class Board:
         self.winner = 0
         self.winner_printed: bool = False
 
-        self.x_sprite = pygame.image.load("sprites/x.png")
-        self.o_sprite = pygame.image.load("sprites/o.png")
+        self.x_sprite = pygame.image.load("./assets/sprites/x.png")
+        self.o_sprite = pygame.image.load("./assets/sprites/o.png")
+
+        self.is_draw = 0
     
+    def reset(self): 
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+
+        self.turn = self.x_enum if self.turn == self.x_enum else self.o_enum
+        self.winner = 0
+        self.winner_printed: bool = False
+
     def draw(self) -> None: 
         row_height: int = (WIDTH/3).__floor__()
         col_width: int = (WIDTH/3).__floor__()
@@ -31,23 +62,34 @@ class Board:
         pygame.draw.line(win, (255, 0, 255), (0, row_height), (WIDTH, row_height))
         pygame.draw.line(win, (255, 0, 255), (0, row_height * 2), (WIDTH, row_height * 2))
 
+        self.is_draw = 0
         for r in range(3):
             for c in range(3): 
                 piece = self.board[r][c]
-
+                if not piece == 0: 
+                    self.is_draw += 1
                 if piece == self.x_enum: 
                     win.blit(self.x_sprite, (c * col_width, r * row_height))
                 elif piece == self.o_enum: 
                     win.blit(self.o_sprite, (c * col_width, r * row_height))
         
+        text = ""
         if self.winner:
-            text = ""
             if self.winner == self.o_enum: 
                 text = "O won!"
             else: 
                 text = "X won!"
-            text_surface = self.font.render(text, False, (255, 0, 255))
+            text_surface = big_font.render(text, False, (255, 0, 255))
+            pygame.draw.rect(win, (255, 255, 255), pygame.Rect(0, HEIGHT-FONT_SIZE, 300, FONT_SIZE))
             win.blit(text_surface, (0, HEIGHT-FONT_SIZE))
+            self.reset_button.draw()
+            
+        elif self.is_draw == 9:
+            text = "Draw!" 
+            text_surface = big_font.render(text, False, (255, 0, 255))
+            pygame.draw.rect(win, (255, 255, 255), pygame.Rect(0, HEIGHT-FONT_SIZE, 300, FONT_SIZE))
+            win.blit(text_surface, (0, HEIGHT-FONT_SIZE))
+            self.reset_button.draw()
 
     def xy_to_rc(self, mouse_x: int, mouse_y: int) -> tuple: 
         row: int = 0
@@ -118,6 +160,8 @@ def main():
                     break
                 case pygame.MOUSEBUTTONDOWN:
                     board.make_move(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+                    if (board.winner or board.is_draw == 9) and board.reset_button.is_clicked(): 
+                        board.reset()
 
         win.fill((0, 0, 0))
 
